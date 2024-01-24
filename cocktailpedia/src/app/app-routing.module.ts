0fe06db8da604fa2cbe310adaf64f6df8f9639a1
Cinthyadelao <118902802+Cinthyadelao@users.noreply.github.com>
2024-01-24 10:35:01 +0100

    add guard

--- cocktailpedia/src/app/app-routing.module.ts
+++ cocktailpedia/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShowCocktailComponent } from './cocktail/show-cocktail/show-cocktail.component';
 import { ListCocktailComponent } from './cocktail/list-cocktail/list-cocktail.component';
+import { authGuard } from './administration/auth.guard';
 
 const routes: Routes = [
   { path: 'admin', loadChildren: () => import('./administration/administration.module').then(m => m.AdministrationModule) },
   { path: 'cocktails', loadChildren: () => import('./cocktail/cocktail.module').then(m => m.CocktailModule) },
-  { path: '', redirectTo: '/cocktails', pathMatch: 'full' }
+  { path: '', redirectTo: '/cocktails', pathMatch: 'full', }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cocktailpedia/src/app/cocktail/cocktail-routing.module.ts
+++ cocktailpedia/src/app/cocktail/cocktail-routing.module.ts
@@ -3,18 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListCocktailComponent } from './list-cocktail/list-cocktail.component';
 import { CreateCocktailComponent } from './create-cocktail/create-cocktail.component';
 import { ShowCocktailComponent } from './show-cocktail/show-cocktail.component';
-
+import { authGuard } from '../administration/auth.guard';
 const routes: Routes = [
     {
         path: 'cocktails', children: [
             { path: 'cocktails', component: ListCocktailComponent },
-            { path: 'create', component: CreateCocktailComponent },
-            { path: 'id', component: ShowCocktailComponent },
-        ]
-    }
-
-
-];
+            { path: 'create', component: CreateCocktailComponent, canActivate: [authGuard] },
+            { path: 'id', component: ShowCocktailComponent }],
+    }];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
